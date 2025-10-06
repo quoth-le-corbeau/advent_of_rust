386c90ba6c24f9e0322eb352265f793896d77589
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-10-06 16:20:28 +0200

    2020 d1p1ex

--- src/aoc_2020/day_1/solution.rs
+++ src/aoc_2020/day_1/solution.rs
@@ -1,2 +1,43 @@
-pub fn part_one() -> i32 {42}
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+pub fn part_one() -> Result<i32, Box<dyn std::error::Error>> {
+    let file_path = "src/aoc/2020/day_1/example.txt";
+    let file = File::open(file_path)?;
+    let reader:BufReader<File> = BufReader::new(file);
+    let lines = BufReader::new(file).lines();
+    for line in lines {
+        println!("{}", line?);
+    }
+    // let mut seen: HashSet = HashSet::new();
+    Ok(3)
+}
 pub fn part_two() -> i32 {128}
+
+
+
+pub fn part_one() -> Result<(u32), Box<dyn std::error::Error>> {
+    let file = File::open("src/day_1/example.txt")?;
+    let reader:BufReader<File> = BufReader::new(file);
+
+    let numbers: Vec<u32> = reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| line.trim().to_string())
+        .filter(|line| !line.is_empty())
+        .filter_map(|line| line.parse().ok())
+        .collect();
+
+    let mut seen = HashSet::new();
+
+    for &number in &numbers {
+        let target: u32 = 2020 - number;
+        if seen.contains(&target) {
+            return Ok((number*target));
+        }
+        seen.insert(number);
+    }
+
+    Err("No pair found".into())
+}
--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn twenty_twenty () {
     println!("-------------");
     println!("2020");
     println!("-------------");
-    let day_1_p1 = aoc_2020::day_1::solution::part_one();
+    let day_1_p1: i32 = aoc_2020::day_1::solution::part_one();
     println!("Day 1 Part 1: {}", day_1_p1);
-    let day_1_p2 = aoc_2020::day_1::solution::part_two();
+    let day_1_p2: i32 = aoc_2020::day_1::solution::part_two();
     println!("Day 1 Part 2: {}", day_1_p2);
 
     println!("-------------");
