cead3bc6c2e4cee4d5798189f58f6140414a0471
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-10-16 16:51:20 +0200

    2016 day 1 part 1

--- src/aoc_2016/day_1/solution.rs
+++ src/aoc_2016/day_1/solution.rs
@@ -1,10 +1,32 @@
 pub fn part_1(file_path: &str) -> Result<u32, Box<dyn std::error::Error>> {
-    let lines: String = parse_input(file_path).unwrap();
-    println!("{lines}");
-    Ok(0)
+    let instructions: Vec<String> = parse_input(file_path).unwrap();
+    let mut unit_vector = (0, -1);
+    let mut position: (i32, i32) = (0, 0);
+    for instruction in instructions {
+        let direction = &instruction[0..1];
+        let distance: u32 = instruction[1..].parse()?;
+        if direction == "R" {
+            unit_vector = (-unit_vector.1, unit_vector.0);
+        }
+        else if direction == "L" {
+            unit_vector = (unit_vector.1, -unit_vector.0);
+        }
+        else {
+            Err("Invalid direction")?;
+        }
+        // Move distance steps in the unit_vector direction
+        position = (
+            position.0 + unit_vector.0 * distance as i32,
+            position.1 + unit_vector.1 * distance as i32,
+        );
+    }
+    let manhattan: i32 = position.0.abs() + position.1.abs();
+    Ok(manhattan as u32)
 }
 
-fn parse_input(file_path: &str) -> Result<String, std::io::Error> {
+fn parse_input(file_path: &str) -> Result<Vec<String>, std::io::Error> {
     let content: String = std::fs::read_to_string(file_path)?;
-    Ok(content)
+    let parts: Vec<String> = content.trim().split(", ").map(|s| s.to_string()).collect();
+    Ok(parts)
 }
+
