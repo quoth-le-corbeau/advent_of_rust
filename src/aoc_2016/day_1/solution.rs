054d08b718f4d47cbc672240698427f8cf9e46ce
quoth-the-macbook <amgorman@tutanota.com>
2025-10-16 22:08:12 +0200

    I fucked up the refactoring

--- src/aoc_2016/day_1/solution.rs
+++ src/aoc_2016/day_1/solution.rs
@@ -3,23 +3,29 @@ pub fn part_1(file_path: &str) -> Result<u32, Box<dyn std::error::Error>> {
     let mut unit_vector = (0, -1);
     let mut position: (i32, i32) = (0, 0);
     for instruction in instructions {
-        let direction = &instruction[0..1];
-        let distance: u32 = instruction[1..].parse()?;
-        if direction == "R" {
-            unit_vector = (-unit_vector.1, unit_vector.0);
-        } else if direction == "L" {
-            unit_vector = (unit_vector.1, -unit_vector.0);
-        } else {
-            Err("Invalid direction")?;
-        }
-        // Move distance steps in the unit_vector direction
-        position = (
-            position.0 + unit_vector.0 * distance as i32,
-            position.1 + unit_vector.1 * distance as i32,
-        );
+       position = move_per_instruction(instruction, &unit_vector, &position);
     }
     let manhattan: i32 = position.0.abs() + position.1.abs();
     Ok(manhattan as u32)
+}
+
+fn move_per_instruction(instruction: String, unit_vector: (i32, i32), position: (i32, i32)) -> Result<(i32, i32), Box<dyn std::error::Error>> {
+    let direction = &instruction[0..1];
+    let distance: i32 = instruction[1..].parse()?;
+    if instruction == "R" {
+        let mut unit_vector = (-unit_vector.1, -unit_vector.0);
+    }
+    else if instruction == "L" {
+        let mut unit_vector = (unit_vector.1, -unit_vector.0);
+    }
+    else {
+        Err("Invalid direction: {direction}")?;
+    }
+    position = (
+        position.0 + unit_vector.0 * distance as i32,
+        position.1 + unit_vector.1 * distance as i32,
+        );
+    Ok(position);
 }
 
 fn parse_input(file_path: &str) -> Result<Vec<String>, std::io::Error> {
