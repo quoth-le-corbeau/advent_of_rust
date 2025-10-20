5606d4b203eb5db100a6c81471394452bc4eae82
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-10-20 10:14:11 +0200

    successfully reimpliment python solution

--- src/aoc_2022/day_7/solution.rs
+++ src/aoc_2022/day_7/solution.rs
@@ -1,23 +1,48 @@
 use std::collections::{HashMap, HashSet};
 use std::path::Path;
 
-pub fn part_1<P: AsRef<Path>>(file_path: P) -> Result<i32, Box<dyn std::error::Error>> {
+const PART_ONE_SIZE_LIMIT: i64 = 100000;
+const TOTAL_DISK_SPACE: i64 = 70000000;
+const SPACE_REQUIRED_FOR_UPDATE: i64 = 30000000;
+
+pub fn part_1<P: AsRef<Path>>(file_path: P) -> Result<i64, Box<dyn std::error::Error>> {
+    let (root_dir_size, size_map) = get_file_system_size(file_path)?;
+    let mut total: i64 = 0;
+    for (dir_name, dir_size) in &size_map {
+       if *dir_size <= PART_ONE_SIZE_LIMIT {
+           total += *dir_size
+       }
+    }
+    Ok(total)
+}
+
+pub fn part_2<P: AsRef<Path>>(file_path: P) -> Result<i64, Box<dyn std::error::Error>> {
+    let (root_dir_size, size_map) = get_file_system_size(file_path)?;
+    let free_disk_space: i64 = TOTAL_DISK_SPACE - root_dir_size;
+    let minimum_space_to_free: i64 = SPACE_REQUIRED_FOR_UPDATE - free_disk_space;
+    let mut candidates: Vec<i64> = Vec::new();
+    for (dir_name, dir_size) in &size_map {
+        if *dir_size >= minimum_space_to_free {
+            candidates.push(*dir_size);
+        }
+    }
+    candidates.sort();
+    let best_candidate: i64 = *candidates.first().ok_or_else(|| "no solution")?;
+    Ok(best_candidate)
+}
+
+fn get_file_system_size<P: AsRef<Path>>(
+    file_path: P,
+) -> Result<(i64, HashMap<String, i64>), Box<dyn std::error::Error>> {
     let tree: HashMap<String, HashSet<String>> = parse_input(file_path)?;
-    let mut dir_size_by_dir_name: HashMap<String,i64> = HashMap::new();
+    let mut dir_size_by_dir_name: HashMap<String, i64> = HashMap::new();
     for (dir_name, _) in &tree {
         dir_size_by_dir_name.insert(dir_name.to_string(), 0);
     }
-    //println!("dir_size_by_dir_name: {:?}", dir_size_by_dir_name);
-    let (root_dir_size, size_map) = dfs_sum(&tree, &dir_size_by_dir_name, &"/".to_string());
-    println!("{}", root_dir_size);
-    println!("{:?}", size_map);
-
-    Ok(-999)
-}
-
-pub fn part_2<P: AsRef<Path>>(file_path: P) -> Result<i32, Box<dyn std::error::Error>> {
-    println!("AoC Day 7 Part2");
-    Ok(-999)
+    let (root_dir_size, size_map) = dfs_sum(&tree, &mut dir_size_by_dir_name, &"/".to_string());
+    // println!("{}", root_dir_size);
+    // println!("{:?}", size_map);
+    Ok((root_dir_size, size_map))
 }
 
 fn parse_input<P: AsRef<Path>>(
@@ -40,8 +65,6 @@ fn build_tree(
     let mut path: Vec<String> = Vec::new();
     let mut current_dir = "/".to_string(); // Tracks the current dir for inserting entries
     for line in lines {
-        let parts: Vec<&str> = line.split_whitespace().collect();
-
         if line == "$ ls" {
             continue;
         } else if line == "$ cd .." {
@@ -70,7 +93,7 @@ fn build_tree(
 
 fn dfs_sum(
     tree: &HashMap<String, HashSet<String>>,
-    size_map: &HashMap<String, i64>,
+    size_map: &mut HashMap<String, i64>,
     node: &str,
 ) -> (i64, HashMap<String, i64>) {
     let mut dir_size: i64 = 0;
@@ -80,21 +103,16 @@ fn dfs_sum(
             parts.next(); // Skip "dir"
 
             if let Some(dir_name) = parts.next() {
-                // Remove trailing slash from node
                 let trimmed_node = node.trim_end_matches('/');
 
-                // Build new path
                 let new_node = format!("{}/{}", trimmed_node, dir_name.trim());
 
-                // Recursive call
-                let (s, _) = dfs_sum(&tree, &size_map, &new_node);
+                let (s, _) = dfs_sum(&tree, size_map, &new_node);
                 dir_size += s;
             }
-        }
-        else {
+        } else {
             let parts: Vec<&str> = child.split(' ').collect();
 
-            // Try to parse the first part as an integer
             let file_size: usize = match parts.get(0).and_then(|s| s.trim().parse().ok()) {
                 Some(size) => size,
                 None => panic!("Invalid file: {}", child),
@@ -103,5 +121,6 @@ fn dfs_sum(
             dir_size += file_size as i64;
         }
     }
+    size_map.insert(node.to_string(), dir_size);
     (dir_size, size_map.clone())
 }
--- src/aoc_2022/mod.rs
+++ src/aoc_2022/mod.rs
@@ -10,33 +10,33 @@ pub fn twenty_twenty_two() {
         Err(e) => println!("Example: Day 7 part 1 Error: {e}"),
     }
     println!("-----------------------------------------------------");
-    //match day_7_part_1() {
-    //    Ok(value) => println!("Day1 part 1 value: {value}"),
-    //    Err(e) => println!("Day 7 part 1 Error: {e}"),
-    //}
-    //println!("-----------------------------------------------------");
-    //match day_7_part_2_eg() {
-    //    Ok(value) => println!("Example: Day 7 part 2 value: {value}"),
-    //    Err(e) => println!("Example: Day 7 part 2 Error: {e}"),
-    //}
-    //println!("-----------------------------------------------------");
-    //match day_7_part_2() {
-    //    Ok(value) => println!("Day 7 part 2 value: {value}"),
-    //    Err(e) => println!("Day 7 part 2 Error: {e}"),
-    //}
-    //println!("-----------------------------------------------------");
+    match day_7_part_1() {
+        Ok(value) => println!("Day1 part 1 value: {value}"),
+        Err(e) => println!("Day 7 part 1 Error: {e}"),
+    }
+    println!("-----------------------------------------------------");
+    match day_7_part_2_eg() {
+        Ok(value) => println!("Example: Day 7 part 2 value: {value}"),
+        Err(e) => println!("Example: Day 7 part 2 Error: {e}"),
+    }
+    println!("-----------------------------------------------------");
+    match day_7_part_2() {
+        Ok(value) => println!("Day 7 part 2 value: {value}"),
+        Err(e) => println!("Day 7 part 2 Error: {e}"),
+    }
+    println!("-----------------------------------------------------");
 }
 
-fn day_7_part_1_eg() -> Result<i32, Box<dyn std::error::Error>> {
+fn day_7_part_1_eg() -> Result<i64, Box<dyn std::error::Error>> {
     day_7::solution::part_1("src/aoc_2022/day_7/example.txt")
 }
-//fn day_7_part_1() -> Result<i32, Box<dyn std::error::Error>> {
-//    day_7::solution::part_1("src/aoc_2022/day_7/input.txt")
-//}
-//
-//fn day_7_part_2_eg() -> Result<i32, Box<dyn std::error::Error>> {
-//    day_7::solution::part_2("src/aoc_2022/day_7/example.txt")
-//}
-//fn day_7_part_2() -> Result<i32, Box<dyn std::error::Error>> {
-//    day_7::solution::part_2("src/aoc_2022/day_7/input.txt")
-//}
+fn day_7_part_1() -> Result<i64, Box<dyn std::error::Error>> {
+    day_7::solution::part_1("src/aoc_2022/day_7/input.txt")
+}
+
+fn day_7_part_2_eg() -> Result<i64, Box<dyn std::error::Error>> {
+    day_7::solution::part_2("src/aoc_2022/day_7/example.txt")
+}
+fn day_7_part_2() -> Result<i64, Box<dyn std::error::Error>> {
+    day_7::solution::part_2("src/aoc_2022/day_7/input.txt")
+}
