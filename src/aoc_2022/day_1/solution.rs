4f65df6f2767013cc83906dadaf0a57b1c984f42
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-10-20 17:16:44 +0200

    day 1 both parts

---
+++ src/aoc_2022/day_1/solution.rs
@@ -0,0 +1,43 @@
+use std::path::Path;
+
+pub fn part_1<P: AsRef<Path>>(file_path: P) -> Result<u32, Box<dyn std::error::Error>> {
+    let groups: Vec<String> = parse_input(file_path)?;
+    let mut max_cal: u32 = 0;
+    for group in groups {
+        let mut total: u32 = 0;
+        for item in group.split_whitespace() {
+            let calories: u32 = item.parse()?;
+            total += calories;
+        }
+        if total > max_cal {
+            max_cal = total;
+        }
+    }
+    Ok(max_cal)
+}
+
+pub fn part_2<P: AsRef<Path>>(file_path: P) -> Result<u32, Box<dyn std::error::Error>> {
+    let groups: Vec<String> = parse_input(file_path)?;
+    let mut totals: Vec<u32> = Vec::new();
+    for group in groups {
+        let mut total: u32 = 0;
+        for item in group.split_whitespace() {
+            let calories:u32 = item.parse()?;
+            total += calories;
+        }
+        totals.push(total);
+    }
+    totals.sort();
+    if totals.len() < 3 {
+        return Err("Insufficient calorie groups".into());
+    }
+    let top_three: Vec<u32> = totals[totals.len() - 3..].to_vec();
+    let top_three_sum: u32 = top_three.iter().sum();
+    Ok(top_three_sum)
+}
+
+fn parse_input<P: AsRef<Path>>(file_path: P) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let input_str: String = std::fs::read_to_string(file_path)?;
+    let groups: Vec<String> = input_str.split("\n\n").map(|s| s.to_string()).collect();
+    Ok(groups)
+}
--- src/aoc_2022/mod.rs
+++ src/aoc_2022/mod.rs
@@ -1,9 +1,32 @@
+pub mod day_1;
 pub mod day_7;
 
 pub fn twenty_twenty_two() {
     println!("-----------------------------------------------------");
     println!("\n=========== *** Advent of Code 2022 *** =============");
     println!("-----------------------------------------------------");
+    println!("------------------- >>> Day 1 <<< -------------------");
+    match day_1_part_1_eg() {
+        Ok(value) => println!("Example: Day 1 part 1 value: {value}"),
+        Err(e) => println!("Example: Day 1 part 1 Error: {e}"),
+    }
+    println!("-----------------------------------------------------");
+    match day_1_part_1() {
+        Ok(value) => println!("Day1 part 1 value: {value}"),
+        Err(e) => println!("Day 1 part 1 Error: {e}"),
+    }
+    println!("-----------------------------------------------------");
+    match day_1_part_2_eg() {
+        Ok(value) => println!("Example: Day 1 part 2 value: {value}"),
+        Err(e) => println!("Example: Day 1 part 2 Error: {e}"),
+    }
+    println!("-----------------------------------------------------");
+    match day_1_part_2() {
+        Ok(value) => println!("Day 1 part 2 value: {value}"),
+        Err(e) => println!("Day 1 part 2 Error: {e}"),
+    }
+    println!("-----------------------------------------------------");
+
     println!("------------------- >>> Day 7 <<< -------------------");
     match day_7_part_1_eg() {
         Ok(value) => println!("Example: Day 7 part 1 value: {value}"),
@@ -27,6 +50,22 @@ pub fn twenty_twenty_two() {
     println!("-----------------------------------------------------");
 }
 
+// Day 1
+fn day_1_part_1_eg() -> Result<u32, Box<dyn std::error::Error>> {
+    day_1::solution::part_1("src/aoc_2022/day_1/example.txt")
+}
+fn day_1_part_1() -> Result<u32, Box<dyn std::error::Error>> {
+    day_1::solution::part_1("src/aoc_2022/day_1/input.txt")
+}
+
+fn day_1_part_2_eg() -> Result<u32, Box<dyn std::error::Error>> {
+    day_1::solution::part_2("src/aoc_2022/day_1/example.txt")
+}
+fn day_1_part_2() -> Result<u32, Box<dyn std::error::Error>> {
+    day_1::solution::part_2("src/aoc_2022/day_1/input.txt")
+}
+
+// Day 7
 fn day_7_part_1_eg() -> Result<i64, Box<dyn std::error::Error>> {
     day_7::solution::part_1("src/aoc_2022/day_7/example.txt")
 }
