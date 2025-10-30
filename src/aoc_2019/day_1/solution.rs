5dac7fe18a5d686fedd5552350731af2aa0f9e28
andrew gorman <andrew.gorman@hydrogrid.ai>
2025-10-30 10:59:40 +0100

    2019 part 1

--- src/aoc_2019/day_1/solution.rs
+++ src/aoc_2019/day_1/solution.rs
@@ -1,6 +1,9 @@
 use std::path::Path;
 pub fn part_1<P: AsRef<Path>>(file_path: P) -> Result<u32, Box<dyn std::error::Error>> {
-    let input_lines: Vec<u32> = std::fs::read_to_string(file_path)?.lines().map(|x| x.parse().unwrap()).collect();
+    let input_lines: Vec<u32> = std::fs::read_to_string(file_path)?
+        .lines()
+        .map(|x| x.parse().unwrap())
+        .collect();
     let mut total = 0;
     for line in input_lines {
         total += (line / 3) - 2;
